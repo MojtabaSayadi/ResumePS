d30bfd605b0c3d41de72c9f6e787f7f4bb5bcfaf
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2025-07-03 12:25:26 +0430

    Add project files.

---
+++ ResumePS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResumePS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
